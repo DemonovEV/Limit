917039b0b4c8aab3bcc7e2e4ee5c38cae0834da2
DemonovEV <edemonov@dev.vtb.ru>
2025-03-05 13:06:21 +0300

    Init

---
+++ src/main/java/org/example/limits/LimitsApplication.java
@@ -0,0 +1,13 @@
+package org.example.limits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimitsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/limits/LimitsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.limits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
